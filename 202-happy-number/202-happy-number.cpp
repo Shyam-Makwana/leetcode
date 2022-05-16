b8375460e0a4b251322a02e80474d0dafa7ea06e
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-05-16 23:41:17 +0530

    Time: 3 ms (57.40%), Space: 5.8 MB (94.99%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    bool isHappy(int n) {
-        unordered_map<int, bool> mp;
-        while(!mp[n]){
-            mp[n] = true;
-            int sum=0;
-            while(n){
-                sum+=(n%10)*(n%10);
-                n/=10;
-            }
-            if(sum==1)  return true;
-            n=sum;
-            cout<<sum<<' ';
+    int digitSquareSum(int n) {
+        int sum = 0, tmp;
+        while (n) {
+            tmp = n % 10;
+            sum += tmp * tmp;
+            n /= 10;
         }
-        return false;
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        int slow, fast;
+        slow = fast = n;
+        do {
+            slow = digitSquareSum(slow);
+            fast = digitSquareSum(fast);
+            fast = digitSquareSum(fast);
+        } while(slow != fast);
+        if (slow == 1) return 1;
+        else return 0;
     }
 };
