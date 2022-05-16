366e1430db91afb0f77bbdb513b572d6df012f17
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-05-16 23:11:53 +0530

    Time: 447 ms (11.50%), Space: 92.6 MB (5.07%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
-        if(grid[0][0] || grid[n-1][n-1])    return -1;
-        int ans = INT_MAX, level = 0;
-        vector <pair<int,int>> dir {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-        queue <pair<int,int>> q;
-        q.push({0, 0});
+        int m = grid.size(), n = grid[0].size();
+        if(grid[0][0] || grid[m-1][n-1])    return -1;
+        queue<vector<int>> q;
+        q.push({0,0,1});
+        grid[0][0]=1;
+        vector<vector<int>> v = { {1,1}, {0,1}, {1,0}, {0,-1}, {-1,0}, {-1,-1}, {-1,1}, {1,-1} };
+        
         while(!q.empty()){
-            int m = q.size();
-            while(m--){
-                pair <int,int> p = q.front();
-                q.pop();
-                int i = p.first ;
-                int j = p.second;
-                if(i<0 || j<0 || i>=n || j>=n || grid[i][j] == 1)
-                    continue;
-                if(i == n-1 && j == n-1){
-                    ans = min(ans, level);
-                    continue;
-                }
-                grid[i][j] = 1;
-                for(auto k:dir){
-                    int x = i + k.first ;
-                    int y = j + k.second;
-                    q.push({x, y});
+            vector<int> temp = q.front();
+            q.pop();
+            
+            if(temp[0] == m-1 && temp[1] == n-1)    return temp[2];
+            
+            for(auto i:v){
+                int x = temp[0] + i[0];
+                int y = temp[1] + i[1];
+                
+                if(x>=0 && y>=0 && x<m && y<n && grid[x][y]==0){
+                    grid[x][y]=1;
+                    q.push({x,y,temp[2]+1});
                 }
             }
-            level ++;
         }
-        return ans == INT_MAX ? -1 : ans+1;
+        return -1;
     }
 };
