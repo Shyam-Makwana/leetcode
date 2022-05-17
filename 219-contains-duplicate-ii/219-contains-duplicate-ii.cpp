2378aad5a784508019c918a3d415c793b17cdca6
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-05-17 12:36:07 +0530

    Time: 333 ms (18.75%), Space: 72.3 MB (90.49%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -4,7 +4,6 @@ public:
         unordered_set<int> s;
         for(int i=0;i<nums.size();i++){
             if(!s.insert(nums[i]).second)   return true;
-            s.insert(nums[i]);
             if(s.size() > k)    s.erase(nums[i-k]);
         }
         return false;
