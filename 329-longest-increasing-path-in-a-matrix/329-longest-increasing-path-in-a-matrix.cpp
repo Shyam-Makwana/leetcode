c2e5c248d1007828e932b3cc63cf381f0a33712b
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-05-19 14:25:04 +0530

    Time: 65 ms (51.05%), Space: 14.8 MB (95.23%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    int moves[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
-    
-    vector<vector<int>> dp;
-    
+    int dp[200][200] = {};
+    int maxPath, n, m;
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int maxPath = 0, n = size(matrix), m = size(matrix[0]);
-	    dp.resize(n, vector<int>(m));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                maxPath = max(maxPath, solve(matrix, i, j));
-            }
-        }
+        maxPath = 0, n = size(matrix), m = size(matrix[0]);
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                maxPath = max(maxPath, solve(matrix, i, j, -1));
         return maxPath;
     }
     
-    int solve(vector<vector<int>>& mat, int i, int j){
+    int solve(vector<vector<int>>& mat, int i, int j, int prev){
+        if(i<0 || j<0 || i>=n || j>=m || mat[i][j]<=prev) return 0;
         if(dp[i][j])    return dp[i][j];
-        dp[i][j] = 1;
-        for(int k=0;k<4;k++){
-            int new_i = i + moves[k][0], new_j = j + moves[k][1];
-            if(new_i<0 || new_j<0 || new_i>=mat.size() || new_j>=mat[0].size() || mat[new_i][new_j] <= mat[i][j])   continue;
-            dp[i][j] = max(dp[i][j], 1 + solve(mat, new_i, new_j));
-        }
-        return dp[i][j];
+        return dp[i][j] = 1 + max({ solve(mat, i+1, j, mat[i][j]),
+                                    solve(mat, i-1, j, mat[i][j]), 
+                                    solve(mat, i, j+1, mat[i][j]),
+                                    solve(mat, i, j-1, mat[i][j]) });
     }
 };
