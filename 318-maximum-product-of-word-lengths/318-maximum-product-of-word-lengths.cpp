97f90e86cab3d3479a1aebe9abf3f4b75c053704
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-05-30 10:34:45 +0530

    Time: 67 ms (61.26%), Space: 16.9 MB (39.62%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    
-    bool checkCommon(bitset<26> &v1, bitset<26> &v2){
-        for(int i=0;i<26;i++)   if(v1[i] && v2[i])  return false;
-        return true;
-    }
-    
     int maxProduct(vector<string>& words){ 
-        int n = words.size(), ans = 0; 
-        vector<bitset<26>> vec(n); 
+        int ans = 0;
+        vector<int> v;
         
-        for(int i=0;i<n;i++){
-            for(auto c: words[i])    vec[i][c-'a'] = 1;
+        for(string s: words){
+            int mask=0;
+            for(int i:s){
+                mask |= (1<<(i-'a'));
+            }
+            v.push_back(mask);
+        }
         
-            for(int j=0;j<i;j++){
-                if(checkCommon(vec[i], vec[j])) 
-                    ans =  max(ans, (int) size(words[i]) * (int) size(words[j]));
+        for(int i=0;i<size(words);i++){
+            for(int j=i+1;j<size(words);j++){
+                if((v[i] & v[j]) == 0)  ans = max(ans, (int) size(words[i]) * (int) size(words[j]));
             }
         }
+        
         return ans;
     }
 };
