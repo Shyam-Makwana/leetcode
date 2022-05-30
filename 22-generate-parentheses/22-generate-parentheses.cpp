ea0546f03b52d633f9e6890159b0889305b378a0
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-05-30 23:29:55 +0530

    Time: 4 ms (71.55%), Space: 14 MB (32.82%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -2,25 +2,16 @@ class Solution {
 public:
     int N;
     vector<string> ans;
-    string str;
     
     vector<string> generateParenthesis(int n) {
         N=n;
-        genPar(0,0);
+        addingpar(0,0, "");
         return ans;
     }
     
-    void genPar(int open, int close){
-        if(open==N && close==N) ans.push_back(str);
-        if(open<N){
-            str+='(';
-            genPar(open+1, close);
-            str.pop_back();
-        }
-        if(open > close){
-            str+=')';
-            genPar(open, close+1);
-            str.pop_back();
-        }
+    void addingpar(int open, int close, string par){
+        if(open == N && close == N) ans.push_back(par);
+        if(open < N)  addingpar(open+1, close, par+"(");
+        if(open > close)    addingpar(open, close+1, par+")");
     }
 };
