b0edcf3c90dd3312357e47a649853d6f3abff350
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-06-13 12:32:24 +0530

    Time: 8 ms (61.32%), Space: 8.6 MB (91.19%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        for(int i = n - 2; i >= 0; i--)
-        {
-            for(int j = 0; j < i + 1; j++)
-            {
-                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1]);
-            }
+        int n = size(triangle);
+        for(int i=n-2; i>=0; --i){
+            for(int j=0; j<i+1; ++j)    triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1]);
         }
         return triangle[0][0];
     }
