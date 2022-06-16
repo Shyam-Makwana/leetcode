1725f620b7ad77089443fa514e1703824c07acf6
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-06-16 19:25:28 +0530

    Time: 1298 ms (43.47%), Space: 216.6 MB (76.56%) - LeetHub

---
+++ 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.cpp
@@ -0,0 +1,56 @@
+struct Node{
+    Node *child[2] = {NULL};
+};
+
+class Solution {
+private:
+    Node *root = new Node();
+    
+public:
+    
+    void insert(int n){
+        Node *curr = root;
+        for(int i=31; i>=0; i--){
+            int bit = (n>>i) & 1;
+            if(!curr->child[bit])    curr->child[bit] = new Node();
+            curr = curr->child[bit];
+        }
+    }
+    
+    int findMaxXor(int n){
+        int ans = 0;
+        Node *curr = root;
+        for(int i=31; i>=0; i--){
+            int bit = (n>>i) & 1;
+            if(curr->child[1-bit]){
+                ans |= (1<<i);
+                curr = curr->child[1-bit];
+            }
+            else    curr = curr->child[bit];
+        }
+        return ans;
+    }
+
+    vector<int> maximizeXor(vector<int>& arr, vector<vector<int>>& queries) {
+        vector<int> ans(queries.size(), 0); 
+        vector<pair<int, pair<int,int>>> offlineQueries; 
+        sort(arr.begin(), arr.end()); 
+        int index = 0;
+        for(auto &it: queries) {
+            offlineQueries.push_back({it[1],{it[0], index++}}); 
+        }
+        sort(offlineQueries.begin(), offlineQueries.end()); 
+        int i = 0; 
+        int n = arr.size(); 
+
+        for(auto &it : offlineQueries) {
+            while(i < n && arr[i] <= it.first) {
+                insert(arr[i]); 
+                i++; 
+            }
+            if(i!=0) ans[it.second.second] = findMaxXor(it.second.first); 
+            else ans[it.second.second] = -1; 
+        }
+        return ans; 
+    }
+};
