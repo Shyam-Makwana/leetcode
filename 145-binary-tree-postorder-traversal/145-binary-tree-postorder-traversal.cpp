30918ab4203973eb4a1029485e779555c5e69568
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-06-17 18:09:12 +0530

    Time: 3 ms (57.35%), Space: 8.5 MB (37.23%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -15,9 +15,36 @@ public:
     
     vector<int> postorderTraversal(TreeNode* root) {
         if(!root)   return ans;
-        postorderTraversal(root->left);
+        
+        //Recursive solution
+        /*postorderTraversal(root->left);
         postorderTraversal(root->right);
         ans.push_back(root->val);
+        return ans;*/
+        
+        //Iterative solution
+        stack<TreeNode*>st;
+        TreeNode *curr = root, *temp;
+        while(!st.empty() || curr){
+            if(curr){
+                st.push(curr);
+                curr = curr->left;
+            }
+            else{
+                temp = st.top()->right;
+                if(!temp){
+                   temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while(!st.empty() && temp==st.top()->right){
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else    curr=temp;
+            }
+        }
         return ans;
     }
 };
