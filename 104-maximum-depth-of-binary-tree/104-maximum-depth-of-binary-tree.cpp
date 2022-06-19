309d2d0cfb8659516fe0d93e83e6a5f75e304a50
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-06-19 20:22:29 +0530

    Time: 20 ms (17.19%), Space: 19 MB (14.35%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -14,11 +14,13 @@ public:
     int maxDepth(TreeNode* root) {
         if(!root)   return 0;
         
+        //Recursive DFS
         /*int left = maxDepth(root->left);
         int right = maxDepth(root->right);
         return 1 + max(left, right);*/
         
-        int level = 0;
+        //BFS
+        /*int level = 0;
         queue<TreeNode*> q;
         q.push(root);
         while(!q.empty()){
@@ -30,6 +32,20 @@ public:
                 if(t->right)    q.push(t->right);
             }
             level += 1;
+        }
+        return level;*/
+        
+        //Iterative DFS
+        int level = 0;
+        stack<pair<TreeNode*, int>> st;
+        st.push({root, 1});
+        while(!st.empty()){
+            TreeNode *node = st.top().first;
+            int depth = st.top().second;
+            st.pop();
+            level = max(level, depth);
+            if(node->left) st.push({node->left, depth+1});
+            if(node->right) st.push({node->right, depth+1});
         }
         return level;
     }
