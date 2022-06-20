4d45c3c75121f27f9d6e10b9b1951d468a874a2b
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-06-20 14:35:12 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (6.07%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -11,17 +11,27 @@
  */
 class Solution {
 public:
-    TreeNode* invert(TreeNode *root){
-        if(root==NULL)  return NULL;
-        TreeNode *left = invert(root->left);
-        TreeNode *right = invert(root->right);
-        root->left = right;
-        root->right = left;
-        return root;
-    }
     
     TreeNode* invertTree(TreeNode* root) {
-        invert(root);
+	    if(root==NULL)  return NULL;
+					
+		//Recursive DFS
+        /*invertTree(root->left);
+        invertTree(root->right);
+        swap(root->left, root->right);
+        return root;*/
+
+		//Iterative BFS
+        stack<TreeNode*> stk;
+        stk.push(root);
+
+        while (!stk.empty()) {
+            TreeNode* node = stk.top();
+            stk.pop();
+            swap(node->left, node->right);
+            if(node->left)  stk.push(node->left);
+            if(node->right) stk.push(node->right);
+        }
         return root;
     }
 };
