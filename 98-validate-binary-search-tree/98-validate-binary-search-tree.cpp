9fe263b21bb19b013117506ddc2530a5122019cf
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-06-24 18:00:56 +0530

    Time: 15 ms (73.79%), Space: 21.7 MB (70.63%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root, TreeNode* minNode, TreeNode* maxNode) {
+        if(!root) return true;
+        if(minNode && root->val <= minNode->val || maxNode && root->val >= maxNode->val)    return false;
+        
+        return isValidBST(root->left, minNode, root) && isValidBST(root->right, root, maxNode);
+    }
+    
+    bool isValidBST(TreeNode* root) {
+        return isValidBST(root, NULL, NULL);
+        
+        //Iterative In Order
+        /*stack<TreeNode*> st;
+        TreeNode *prev = NULL;
+        while(root || !st.empty()){
+            while(root){
+                st.push(root);
+                root = root->left;
+            }
+            root = st.top();
+            st.pop();
+            if(prev && (prev->val >= root->val))  return false;
+            prev = root;
+            root = root->right;
+        }
+        return true;*/
+    }
+};
