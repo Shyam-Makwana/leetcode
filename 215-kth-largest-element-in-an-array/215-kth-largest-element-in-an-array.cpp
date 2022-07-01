49df1c7e760728f899b186085e4ab31690598ba2
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-07-01 22:33:08 +0530

    Time: 243 ms (5.01%), Space: 9.9 MB (86.05%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -7,11 +7,32 @@ public:
         /*partial_sort(nums.begin(), nums.begin() + k, nums.end(), greater<int>());
         return nums[k - 1];*/
 
-        priority_queue<int, vector<int>, greater<int>> pq;
+        /*priority_queue<int, vector<int>, greater<int>> pq;
         for(int i: nums){
             pq.push(i);
             if(pq.size()>k) pq.pop();
         }
-        return pq.top();
+        return pq.top();*/
+        
+        int n = size(nums), l = 0, r = n-1;
+        
+        while(true){
+            int pivot = partition(nums, l , r);
+            if(pivot == n - k)  return nums[pivot];
+            else if(pivot < n-k)    l = pivot + 1;
+            else r = pivot - 1;
+        }
+    }
+    
+    int partition(vector<int> &nums, int l, int r){
+        int pivot = l;
+        for(int i=l; i<r; i++){
+            if(nums[i]<=nums[r]){
+                swap(nums[i], nums[pivot]);
+                pivot++;
+            }
+        }
+        swap(nums[pivot], nums[r]);
+        return pivot;
     }
 };
