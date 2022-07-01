221517db9346072e72320c7b4f994ed6240c5f1c
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-07-01 13:37:35 +0530

    Time: 69 ms (55.51%), Space: 17.1 MB (34.85%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
-    int maximumUnits(vector<vector<int>>& B, int T) {
-        int maxUnits = 0, size=0;
-        priority_queue<pair<int, int>> pq;
-        for(auto i: B){
-            pq.push({i[1], i[0]});
+    static bool compare(vector<int> &a, vector<int> &b){
+        return a[1] > b[1];
+    }
+    
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(), boxTypes.end(), compare);
+        
+        int maxUnits = 0;
+        for(auto box: boxTypes){
+            int size = min(box[0], truckSize);
+            truckSize -= size;
+            maxUnits += (size*box[1]);
+            if(!size)   break;
         }
+        return maxUnits;
+        
+        /*int maxUnits = 0, size=0;
+        priority_queue<pair<int, int>> pq;
+        for(auto i: B)  pq.push({i[1], i[0]});
         while(!pq.empty()){
             auto i = pq.top(); pq.pop();
             if((size+i.second)<=T){
@@ -17,6 +30,6 @@ public:
                 break;
             }
         }
-        return maxUnits;
+        return maxUnits;*/
     }
 };
