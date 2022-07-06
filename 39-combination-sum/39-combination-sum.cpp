f209f9efd7d531c87db9dd7869124f66669308c0
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-07-06 11:11:46 +0530

    Time: 7 ms (86.55%), Space: 11.3 MB (53.08%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> comb;
-    
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> temp;
-        combinationSum(candidates, temp, 0, target, 0);
-        return comb;
-    }
-    
-    void combinationSum(vector<int>& candidates, vector<int>& temp, int i, int target, int sum){
-        if(sum==target) {
-            comb.push_back(temp);
-            return;
-        }
-        else if(sum>target || i>=size(candidates)) return;
-        
-        temp.push_back(candidates[i]);
-        combinationSum(candidates, temp, i, target, sum + candidates[i]);
-        
-        temp.pop_back();
-        combinationSum(candidates, temp, i+1, target, sum);
-    }
+	vector<vector<int>> result;
+	
+	vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+		vector<int> current;
+		combinationSum(candidates, current, 0, target, 0);
+		return result;
+	}
+	
+	void combinationSum(vector<int>& candidates, vector<int>& current, int i, int target, int total){
+		if(total==target) {
+			result.push_back(current);
+			return;
+		}
+		else if(total>target) return;
+		
+        for(int j=i; j<size(candidates); j++){
+            current.push_back(candidates[j]);
+            combinationSum(candidates, current, j, target, total + candidates[j]);
+            current.pop_back();
+        }	
+	}
 };
