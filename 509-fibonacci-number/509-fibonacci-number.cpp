4db1f3409eeaa4c70ae30fc365a5e658b535f226
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-07-06 11:33:55 +0530

    Time: 3 ms (60.76%), Space: 5.9 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0)    return 0;
-        if(n==1)    return 1;
+        if(n < 2)   return n;
         int a = 0, b = 1;
         for(int i=2; i<=n; i++){
             int t = a + b;
