e31a3b6b07698e5cf13658bf71b0d62ff191659a
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-07-07 18:06:35 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (89.89%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -5,7 +5,7 @@ public:
     
         bool table[s1.length()+1][s2.length()+1];
 
-        for(int i=0; i<s1.length()+1; i++)
+        for(int i=0; i<s1.length()+1; i++){
             for(int j=0; j< s2.length()+1; j++){
                 if(i==0 && j==0)
                     table[i][j] = true;
@@ -16,6 +16,7 @@ public:
                 else
                     table[i][j] = (table[i-1][j] && s1[i-1] == s3[i+j-1] ) || (table[i][j-1] && s2[j-1] == s3[i+j-1] );
             }
+        }
         return table[s1.length()][s2.length()];
     }
 };
