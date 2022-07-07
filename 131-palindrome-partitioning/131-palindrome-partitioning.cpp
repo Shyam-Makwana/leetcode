c598f4c1f5e28dbaf341d3d79cc48ab860e323d4
Shyam Makwana <77334892+Shyam-Makwana@users.noreply.github.com>
2022-07-07 19:28:10 +0530

    Time: 162 ms (70.11%), Space: 57.6 MB (62.09%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,35 +1,42 @@
+/*
+    Given a string, partition such that every substring is a palindrome, return all possible ones
+    Ex. s = "aab" -> [["a","a","b"],["aa","b"]], s = "a" -> [["a"]]
+
+    Generate all possible substrings at idx, if palindrome potential candidate, backtrack after
+
+    Time: O(n x 2^n)
+    Space: O(n)
+*/
+
 class Solution {
 public:
-    vector<vector<string>> result;
-    
     vector<vector<string>> partition(string s) {
         vector<string> curr;
-        partition(curr, s, 0);
+        vector<vector<string>> result;
+        dfs(s, 0, curr, result);
         return result;
     }
     
-    void partition(vector<string> &curr, string s, int i){
-        if(i==s.size()){
+private:
+    void dfs(string s, int start, vector<string>& curr, vector<vector<string>>& result) {
+        if (start == s.size()) {
             result.push_back(curr);
             return;
         }
-        
-        for(int j=i; j<s.size(); j++){
-            string t = s.substr(i, j+1-i);
-            if(isPal(t)){
-                curr.push_back(t);
-                partition(curr, s, j+1);
+        for (int i = start; i < s.size(); i++) {
+            if (isPalindrome(s, start, i)) {
+                string str = s.substr(start, i - start + 1);
+                curr.push_back(str);
+                dfs(s, i + 1, curr, result);
                 curr.pop_back();
             }
         }
     }
-    
-    bool isPal(string t){
-        int l=0, r=t.size()-1;
-        while(l<r){
-            if(t[l]!=t[r])  return false;
-            l++;
-            r--;
+    bool isPalindrome(string s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right])    return false;
+            left++;
+            right--;
         }
         return true;
     }
