cda9531a097bbd8482bed61e96bf9f951a900c7b
Shyam Makwana <makwanashyam6@gmail.com>
2022-07-08 22:17:34 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (60.11%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -5,24 +5,20 @@ public:
     vector<string> result;
     
     vector<string> letterCombinations(string digits) {
-        if(digits.size()==0)    return result;
-        string curr = "";
-        backTrack(digits, curr, 0, 0);
+        if(digits.empty())    return result;
+        string curr;
+        backTrack(digits, curr, 0);
         return result;
     }
     
-    void backTrack(string digits, string &curr, int i, int cnt){
-        if(cnt==digits.size()){
+    void backTrack(string digits, string curr, int i){
+        if(curr.size()==digits.size()){
             result.push_back(curr);
             return;
         }
         
-        for(int j=i; j<digits.size(); j++){
-            for(int k=0; k<mp[digits[j]].size(); k++){
-                curr += mp[digits[j]][k];
-                backTrack(digits, curr, j+1, cnt+1);
-                curr.pop_back();
-            }
+        for(char c: mp[digits[i]]){
+            backTrack(digits, curr + c, i+1);
         }
     }
 };
