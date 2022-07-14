3182836b5f1f7aab975a87de877c193277a260bb
Shyam Makwana <makwanashyam6@gmail.com>
2022-07-14 22:18:20 +0530

    Time: 67 ms (30.76%), Space: 18.2 MB (13.03%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
+    //BFS Solution
     int numIslands(vector<vector<char>>& grid) {
+        int n = size(grid), m = size(grid[0]), islands = 0, dir[] = {0, 1, 0, -1, 0};
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j]=='1'){
+                    islands++;
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    grid[i][j] = '0';
+                    while(!q.empty()){
+                        pair<int, int> p = q.front();
+                        q.pop();
+                        for(int k=0; k<4; k++){
+                            int r = p.first + dir[k], c = p.second + dir[k+1];
+                            if(r<0 || c<0 || r==n || c==m || grid[r][c]=='0')   continue;
+                            q.push({r, c});
+                            grid[r][c] = '0';
+                        }
+                    }
+                }
+            }
+        }
+        
+        return islands;
+    }
+    
+    //DFS Solution
+    /*int numIslands(vector<vector<char>>& grid) {
         int islands = 0;
         for(int i=0; i<size(grid); i++){
             for(int j=0; j<size(grid[0]); j++){
@@ -21,5 +49,5 @@ public:
         eraseIslands(grid, i-1, j);
         eraseIslands(grid, i, j+1);
         eraseIslands(grid, i, j-1);
-    }
+    }*/
 };
