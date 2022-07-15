e8fe9aacf72fd3115d3c0077303b62f529092adb
Shyam Makwana <makwanashyam6@gmail.com>
2022-07-15 13:29:55 +0530

    Time: 7 ms (79.66%), Space: 8.5 MB (85.66%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -25,6 +25,30 @@ public:
     
     Node* cloneGraph(Node* node) {
         if(node==NULL)  return NULL;
+        
+        mp[node] = new Node(node->val);
+        
+        queue<Node*> q;
+        q.push(node);
+        
+        while(!q.empty()){
+            Node *curr = q.front();
+            q.pop();
+            
+            for(auto it: curr->neighbors) {
+                if(mp.find(it)==mp.end()){
+                    mp[it] = new Node(it->val);
+                    q.push(it);
+                }
+                mp[curr]->neighbors.push_back(mp[it]);
+            }
+        }
+        return mp[node];
+    }
+    
+    /*DFS Solution
+    Node* cloneGraph(Node* node) {
+        if(node==NULL)  return NULL;
         //cout<<node->val<<' ';
         if(mp.find(node) == mp.end()) {
             mp[node] = new Node(node->val);
@@ -34,7 +58,6 @@ public:
                 mp[node]->neighbors.push_back(cloneGraph(it));
             }
         }
-        
         return mp[node];
-    }
+    }*/
 };
