0da75f841cb2f6331b1d51119850a74f90932e94
Shyam Makwana <makwanashyam6@gmail.com>
2022-07-15 14:44:25 +0530

    Time: 15 ms (97.12%), Space: 23.2 MB (67.07%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -5,23 +5,21 @@ public:
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(grid[i][j]) {
-                    int area = 0;
-                    dfs(grid, i, j, area);
-                    maxArea = max(maxArea, area);
+                    maxArea = max(maxArea, dfs(grid, i, j));
                 }
             }
         }
         return maxArea;
     }
     
-    void dfs(vector<vector<int>>& grid, int i, int j, int &area){
-        if(i<0 || j<0 || i==size(grid) || j==size(grid[0]) || !grid[i][j])  return;
+    int dfs(vector<vector<int>>& grid, int i, int j){
+        if(i<0 || j<0 || i==size(grid) || j==size(grid[0]) || !grid[i][j])  return 0;
         
-        area++;
         grid[i][j] = 0;
-        dfs(grid, i+1, j, area);
-        dfs(grid, i-1, j, area);
-        dfs(grid, i, j+1, area);
-        dfs(grid, i, j-1, area);
+        
+        return (1 + dfs(grid, i+1, j) +
+                    dfs(grid, i-1, j) +
+                    dfs(grid, i, j+1) +
+                    dfs(grid, i, j-1) );
     }
 };
