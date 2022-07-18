081d56d7e88bd9723b051d61cf92421b1cf39e3a
Shyam Makwana <makwanashyam6@gmail.com>
2022-07-18 10:52:46 +0530

    Time: 7 ms (89.25%), Space: 6.5 MB (90.53%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,22 +1,24 @@
+const int MOD = 1E9+7;
 class Solution {
 public:
+    int dp[50][50][51];
     int m, n;
-    int memo[50][50][51];
-    int DIR[5] = {0, 1, 0, -1, 0};
+    int dir[5] = {0, 1, 0, -1, 0};
     
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        this->m = m; this->n = n;
-        memset(memo, -1, sizeof(memo));
-        return dp(startRow, startColumn, maxMove);
+        this->m = m, this->n = n;
+        memset(dp, -1, sizeof(dp));
+        return dfs(startRow, startColumn, maxMove);
     }
     
-    int dp(int r, int c, int maxMove) {
-        if (r < 0 || r == m || c < 0 || c == n) return 1; // Out of bound -> Count 1 way
-        if (maxMove == 0) return 0;
-        if (memo[r][c][maxMove] != -1) return memo[r][c][maxMove];
+    int dfs(int r, int c, int maxMove) {
+        if(r<0 || c<0 || r==m || c==n)  return 1;
+        if(maxMove == 0) return 0;
+        if(dp[r][c][maxMove] != -1) return dp[r][c][maxMove];
+        
         int ans = 0;
         for (int i = 0; i < 4; ++i)
-            ans = (ans + dp(r + DIR[i], c + DIR[i+1], maxMove - 1)) % 1000000007;
-        return memo[r][c][maxMove] = ans;
+            ans = (ans + dfs(r + dir[i], c + dir[i+1], maxMove - 1)) % MOD;
+        return dp[r][c][maxMove] = ans;
     }
 };
