1a9715982da1a142edbd9f260943ee12eb3aba8b
Shyam Makwana <makwanashyam6@gmail.com>
2022-08-02 11:22:32 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (53.26%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> grid(m, vector<int>(n, 0));
         vector<vector<int>> dp(m, vector<int>(n, 0));
-        return dfs(0, 0, grid, dp);
+        return dfs(0, 0, dp);
     }
     
-    int dfs(int m, int n, vector<vector<int>> &grid, vector<vector<int>> &dp){
-        int row = size(grid), col = size(grid[0]);
-        if(m<0 || n<0 || m==row || n==col || grid[m][n]) return 0;
+    int dfs(int m, int n, vector<vector<int>> &dp){
+        int row = size(dp), col = size(dp[0]);
+        if(m==row || n==col) return 0;
         if(dp[m][n])    return dp[m][n];
         if(m==row-1 && n==col-1)    return 1;
-        
-        grid[m][n] = 1;
-        dp[m][n] = dfs(m+1, n, grid, dp) + dfs(m, n+1, grid, dp);
-        grid[m][n] = 0;
+
+        dp[m][n] = dfs(m+1, n, dp) + dfs(m, n+1, dp);
         
         return dp[m][n];
     }
