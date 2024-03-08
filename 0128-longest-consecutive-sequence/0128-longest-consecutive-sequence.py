d0185c1c0806965a06193585c9c9223b15f49cd1
Shyam Makwana <makwanashyam6@gmail.com>
2024-03-08 17:58:43 -0500

    Time: 4659 ms (12.26%), Space: 34.8 MB (13.00%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -7,11 +7,9 @@ class Solution:
         ans = 0
         for n in nums:
             if n-1 not in hash_map:
-                count = n
                 length = 1
-                while count + 1 in hash_map:
+                while n + length in hash_map:
                     length += 1
-                    count += 1
                 
                 ans = max(ans, length)
         
