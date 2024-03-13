9f4db5e50ba0a93bcae7ac2bd03b032625da7b42
Shyam Makwana <makwanashyam6@gmail.com>
2024-03-12 20:35:10 -0400

    Time: 315 ms (5.26%), Space: 16.6 MB (91.30%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -12,12 +12,11 @@ class Solution:
         while r < len(s):
             freq[s[r]] = 1 + freq.get(s[r], 0)
             maxx = self.findMaxChar(freq)
-            if ((r-l+1) - maxx) <= k:
-                result = max(result, r-l+1)
-                r += 1
-            else:
-                freq[s[l]]-=1
+            while ((r-l+1) - maxx) > k:
+                freq[s[l]] -= 1
+                maxx = self.findMaxChar(freq)
                 l+=1
-                r+=1
+            result = max(result, r-l+1)
+            r += 1
         return result
         
