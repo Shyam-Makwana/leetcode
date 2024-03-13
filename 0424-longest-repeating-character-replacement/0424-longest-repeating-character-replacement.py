aa1b99fb8966dbcb685de1dd370f982562e396cb
Shyam Makwana <makwanashyam6@gmail.com>
2024-03-12 20:42:51 -0400

    Time: 85 ms (68.37%), Space: 16.6 MB (91.30%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,21 +1,20 @@
 class Solution:
-    def findMaxChar(self, freq: dict) -> int:
-        maxx = 0
-        for k, v in freq.items():
-            maxx = max(maxx, v)
-        return maxx
-    
     def characterReplacement(self, s: str, k: int) -> int:
         freq = {}
         result = 0
         l, r = 0, 0
+        maxf = 0
         while r < len(s):
             freq[s[r]] = 1 + freq.get(s[r], 0)
-            maxx = self.findMaxChar(freq)
-            while ((r-l+1) - maxx) > k:
+            
+            maxf = max(maxf, freq[s[r]])
+            
+            while ((r-l+1) - maxf) > k:
                 freq[s[l]] -= 1
                 l+=1
+                
             result = max(result, r-l+1)
             r += 1
+            
         return result
         
